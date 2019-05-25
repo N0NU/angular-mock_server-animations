4a54aed07f333e162d154c864474ee4a5cb79320
unknown <admin@IT-7.BIOREV.STUDIO>
2019-05-25 13:41:07 +0530

    asdf

--- src/app/main-page/main-page.component.ts
+++ src/app/main-page/main-page.component.ts
@@ -1,15 +1,49 @@
 import { Component, OnInit } from '@angular/core';
+import { MockServiceService } from '../services/mock-service.service';
+import { slideIn } from './slide'; 
+import { rotatedState }  from './rotate'
+
 
 @Component({
   selector: 'app-main-page',
   templateUrl: './main-page.component.html',
-  styleUrls: ['./main-page.component.css']
+  styleUrls: ['./main-page.component.css'],
+  animations: [
+    slideIn,
+    rotatedState
+  ]
 })
 export class MainPageComponent implements OnInit {
 
-  constructor() { }
+  constructor(private mockService: MockServiceService) { }
+cards:any;
+slideInState = 'in';
+  max:number=1;
+  state: string = 'default';
 
+    rotate() {
+        this.state = (this.state === 'default' ? 'rotated' : 'default');
+    }
+  slideIn(){
+  this.slideInState = 'in';
+
+  }
+  slideOut(){
+    this.slideInState = 'out';
+  
+   }
+   next(): void {
+    this.max =this.max+1;
+  }
+  back(): void {
+    this.max =this.max-1;
+    this.slideInState = 'out';
+  }
   ngOnInit() {
+    this.mockService.getCards().subscribe(res=>{
+      this.cards = res;
+      console.log(res,'res');
+    })
   }
 
 }
--- src/app/services/mock-service.service.ts
+++ src/app/services/mock-service.service.ts
@@ -1,9 +1,22 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MockServiceService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getCards(){
+    return this.http.get('http://localhost:3000/cards');
+  }
+
+  addCard(data){
+    return this.http.post('http://localhost:3000/cards', data)
+  }
+
+  deleteCard(id){
+    return this.http.delete('http://localhost:3000/cards', id);
+  }
 }
